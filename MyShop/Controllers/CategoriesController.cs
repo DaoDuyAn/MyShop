f5fa89843e619e2fb09f4eea328cbb5d427d059f
Đào Duy An <daoduyan8801@gmail.com>
2024-02-24 16:39:51 +0700

    Add project files.

---
+++ MyShop/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Entities;
+
+namespace MyShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly MyShopDBContext _context;
+
+        public CategoriesController(MyShopDBContext ctx)
+        {
+            _context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Categories.ToList());
+        }
+    }
+}
