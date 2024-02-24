5e5b60ebf47289405fd62bd591c597b344176ca0
Đào Duy An <daoduyan8801@gmail.com>
2024-02-24 16:55:57 +0700

    feat: add CORS

--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -11,10 +11,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MyShopDBContext>(option => 
     option.UseSqlServer(builder.Configuration.GetConnectionString("MyShop")));
 
-//builder.Services.AddCors(p => p.AddPolicy("MyCors", build => {
-//    //build.WithOrigins("https://hienlth.info", "https://localhost:3000");
-//    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-//}));
+// CORS
+builder.Services.AddCors(p => p.AddPolicy("MyCors", build =>
+{
+    //build.WithOrigins("http://example.com", "https://localhost:3000");
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -27,28 +29,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCors("MyCors");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
