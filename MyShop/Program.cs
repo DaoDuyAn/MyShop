01c3e2be21bb4f2b001272f62bb99f1cab4cb8d9
Đào Duy An <daoduyan8801@gmail.com>
2024-02-27 15:49:26 +0700

    fix: update connection string

--- MyShop/Program.cs
+++ MyShop/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddAuthorization();
+builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
